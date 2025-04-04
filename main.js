c522a7e0658e607c0cf90d5a2ef9a695874a09cd
abrahan <pacha69x@gmail.com>
2025-04-04 17:03:31 -0400

    punton de guardado

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 const contenedor = document.getElementById('contenedor')
 
 
-function peticion (informacion) {
+function peticion(informacion) {
     console.log(informacion)
-    
+
     const musicas = informacion.data.songs
-    
+
+    musicas.map(renderizarMusica)
+
 }
 
 
 
-    function renderizarMusica () {
+function renderizarMusica(informacion) {
+    const div = document.createElement('div')
+    // div.setAttribute
 
-    }
-    
+    div.innerHTML = `
+        <img src="/assets/image 20.png" alt="">
+            <p>titulo</p>
+            <p>artistta</p>
+    `
+
+
+
+
+
+
+contenedor.appendChild(div)
+
+
+
+}
+
 
 
 axios.get("https://api.institutoalfa.org/api/songs")
-     .then(peticion)
+    .then(peticion)
